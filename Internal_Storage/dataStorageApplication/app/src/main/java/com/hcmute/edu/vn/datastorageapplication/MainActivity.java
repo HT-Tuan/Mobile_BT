1b92b50da23d793a348d1db9f36fe0a60fd62c9e
HT-Tuan <huynhthanhtuan2206@gmail.com>
2023-03-31 14:33:49 +0700

    bt luu tru

---
+++ Internal_Storage/dataStorageApplication/app/src/main/java/com/hcmute/edu/vn/datastorageapplication/MainActivity.java
@@ -0,0 +1,103 @@
+package com.hcmute.edu.vn.datastorageapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private String filename = "demoFile.txt";
+    Button read, write;
+    EditText userInput;
+    TextView fileContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        read = (Button) findViewById(R.id.read_button);
+        write = (Button) findViewById(R.id.write_button);
+        userInput = (EditText) findViewById(R.id.userInput);
+        fileContent = (TextView) findViewById(R.id.content);
+
+        read.setOnClickListener(this);
+        write.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button b = (Button) view;
+
+        // get the button text : in out case either read or
+        // write depending on the button pressed.
+        String b_text = b.getText().toString();
+
+        switch (b_text.toLowerCase()) {
+            case "write": {
+                writeData();
+                break;
+            }
+            case "read": {
+                readData();
+                break;
+            }
+        }
+    }
+    private void readData()
+    {
+
+        try
+        {
+            FileInputStream fin = openFileInput(filename);
+            int a;
+            StringBuilder temp = new StringBuilder();
+            while ((a = fin.read()) != -1)
+            {
+                temp.append((char)a);
+            }
+
+            // setting text from the file.
+            fileContent.setText(temp.toString());
+            fin.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        printMessage("reading to file " + filename + " completed..");
+    }
+
+    private void writeData()
+    {
+        try
+        {
+            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            String data = userInput.getText().toString();
+            fos.write(data.getBytes());
+            fos.flush();
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        userInput.setText("");
+        printMessage("writing to file " + filename + "completed...");
+    }
+
+    public void printMessage(String m) {
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+}
